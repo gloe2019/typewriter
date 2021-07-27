16247efdc624eb6c12ec5aaa6217b04a0537d89c
Gloria Edem <gloedem@gmail.com>
2021-07-27 17:23:00 +0000

    refactored code

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,14 @@
 const sentence = "hello there from lighthouse labs";
 
-[...sentence].forEach((char, index) => {
+const typewriter = (message) => {
+  [...message].forEach((char, index) => {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, 50 * index);
+  });
   setTimeout(() => {
-    process.stdout.write(char);
-  }, 50 * index);
-});
-setTimeout(() => {
-  console.log("\n");
-}, 2500);
+    console.log("\n");
+  }, 2500);
+};
+
+typewriter(sentence);
